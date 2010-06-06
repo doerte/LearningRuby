9336b5e484b3f97af00fe2fc27295bc47af7762a
Dörte Hessler <me@doerte.eu>
2010-06-07 00:02:21 +0200

    aangepast voor vraag naar input

--- 10Ex1.rb
+++ 10Ex1.rb
@@ -23,6 +23,21 @@ def rec_sort unsorted, sorted
 	rec_sort still_unsorted, sorted
 end
 
-puts(sort(['can','feel','singing','like','a','can']))
+#puts(sort(['can','feel','singing','like','a','can']))
 
 
+puts 'Please enter as many words as you like, one word at a time finishing with "enter". Once you\'re done, press "enter" after an empty line!'
+puts 'You will then get an alphabetically ordered list of the words you entered'
+
+words = []
+
+while true
+input = gets.chomp
+  if input == ''
+    break	
+  else
+    words.push input
+  end	
+end
+puts 'Here\'s your sorted list:'
+puts sort(words)
