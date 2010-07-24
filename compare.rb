5aa7dcd622f2dc8a698aaf820839415973b3d896
Dörte Hessler <me@doerte.eu>
2010-07-24 16:41:27 +0200

    added map command

--- compare.rb
+++ compare.rb
@@ -18,9 +18,7 @@ fh.each { |line|
 fh.close
 out.close
 
-
 references = ["references.txt"]
-
 
 references.each do |ref|
   text = File.read(ref)
@@ -37,66 +35,20 @@ references.each do |ref|
   text = text.gsub(/,/,"")
   out.puts text
   out.close
-  end
+end
+
 
 # get list of pdf files from directory
-
-basedir = '.'
-articles = Dir.glob("*.pdf")
+article_dir = "D:/Texte/*.pdf"
+articles = Dir[article_dir]
 articles = articles.sort
+articles = articles.map do |entry|
+  File.basename(entry, ".pdf")
+end
+
 out = File.open("D:/Texte/articles.txt", "w")
 out.puts articles
-
-
-#article_dir = "D:/Texte/*.pdf"
-#articles = Dir[article_dir]
-
-#def nopath(filenames)
-#	articles_and_paths = filenames.map do |s|
-#	[s, s.split("/")] # [file, path]
-#	end
-#end
-
-
-#list = nopath(articles)
-#list = list.sort
-
-#out = File.open("articlesA.txt","w")
-#out.puts list
-#out.close
-
-#fh = File.open("articlesA.txt","r");
-#out = File.open("articlesB.txt","w");
-#fh.each { |line|
-#	out.puts line unless line =~ /D:/
-#}
-#fh.close
-#out.close
-
-#fh = File.open("articlesB.txt","r");
-#out = File.open("articlesC.txt","w");
-#fh.each { |line|
-#	out.puts line unless line =~ /Texte/
-#}
-#fh.close
-#out.close
-
-
-#file_names = ["articlesC.txt"]
-#file_names = file_names.sort
-
-#file_names.each do |file_name|
-#  text = File.read(file_name)
-#  out = File.open("D:/Texte/articles.txt", "w")
-#  out.puts text.gsub(/.pdf/, "") 
-#end
-  
-#out.close
-
-
-#File.delete("articlesA.txt")
-#File.delete("articlesB.txt")
-#File.delete("articlesC.txt")
+out.close
 
 
 #get list of copied articles
