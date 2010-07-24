d2609ecbc0ad93f433c210b2b90a5b53dd625809
Dörte Hessler <me@doerte.eu>
2010-07-24 18:15:17 +0200

    reduced a lot: replaced seperate searches with one regular expression

--- ArticleList.rb
+++ ArticleList.rb
@@ -1,45 +1,11 @@
-article_dir = 'D:/Texte/*.pdf'
-
+# get list of pdf files from directory
+article_dir = "D:/Texte/*.pdf"
 articles = Dir[article_dir]
-
-def nopath(filenames)
-	articles_and_paths = filenames.map do |s|
-		[s, s.split('/')] # [song, path]
-	end
+articles = articles.sort
+articles = articles.map do |entry|
+  File.basename(entry, ".pdf")
 end
 
-
-list = nopath(articles)
-
-out = File.open("articlesA.txt","w")
-out.puts list
+out = File.open("D:/Texte/articles.txt", "w")
+out.puts articles
 out.close
-
-fh = File.open("articlesA.txt","r");
-out = File.open("articlesB.txt","w");
-fh.each { |line|
-	out.puts line unless line =~ /D:/
-}
-fh.close; out.close;
-
-fh = File.open("articlesB.txt","r");
-out = File.open("articlesC.txt","w");
-fh.each { |line|
-	out.puts line unless line =~ /Texte/
-}
-fh.close; out.close;
-
-
-file_names = ["articlesC.txt","mapA.txt","books.txt"]
-
-file_names.each do |file_name|
-  text = File.read(file_name)
-  out = File.open('D:/Texte/articles.txt', "w")
-  out.puts text.gsub(/.pdf/, "") 
-end
-
-
-
-File.delete('articlesA.txt')
-File.delete('articlesB.txt')
-File.delete('articlesC.txt')
