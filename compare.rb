82ecc98d5cc784e3343bb7433a1f8cdaae3bae57
doerte <me@doerte.eu>
2010-07-26 14:58:37 +0200

    extended by automatic classification of copies into folders, furthermore clean-up double stuff!

--- compare.rb
+++ compare.rb
@@ -1,72 +1,129 @@
-# get list of citations from bib-tex file
-fh = File.open("D:/Latex/diss.bib","r");
-out = File.open("references.txt","w");
+###### Get list of citations from bib-tex file #######
 
-fh.each do |entry|
-  if entry =~ /^@/
-	out.puts entry	  
-  end
-end
+#get array with identifiers (complete line including identifier)
+input = IO.readlines("D:/Latex/diss.bib")
+ident = []
+input.each {|entry|
+	if entry =~ /^@/
+		ident.push entry
+	end
+}
 
-fh.close
-out.close
+#clean up array to leave only identifiers
+ident = ident.each  {|ref|
+	ref.sub!(/@.*{/, "") 
+	ref.sub!(/,/,"")
+}
 
-
-references = IO.readlines("references.txt")
-
-references = references.each  do |ref|
-ref.sub!(/@.*{/, "") 
-ref.sub!(/,/,"")
-end
-	
+#write to file	
 out = File.open("references.txt", "w")
-out.puts references.sort
+out.puts ident.sort
 out.close
 
 
-# get list of pdf files from directory
-article_dir = "D:/Texte/*.pdf"
-articles = Dir[article_dir]
-articles = articles.map do |entry|
-  File.basename(entry, ".pdf")
-end
+###### Get list of pdf files from directory  and save to file########
+pdf_dir = "D:/Texte/*.pdf"
+pdfs = Dir[pdf_dir]
+pdfs = pdfs.map {|entry|
+	File.basename(entry, ".pdf")
+}
 
-out = File.open("D:/Texte/articles.txt", "w")
-out.puts articles.sort
+out = File.open("pdfs.txt", "w")
+out.puts pdfs.sort
 out.close
 
 
-#get list of copied articles
-copies = IO.readlines("books.txt") + IO.readlines("mapA.txt") + IO.readlines("mapB.txt")
+###### Generate overview of copies from diss.bib ##############
+
+#get array with file information
+input = IO.readlines ("D:/Latex/diss.bib")
+	
+files = []
+input.each {|entry|
+	if entry =~ /^  file/
+		if entry =~ /^.*mapA/
+			files.push "mapA"
+		elsif entry =~ /^.*mapB/
+			files.push "mapB"
+		elsif entry =~ /^.*books/
+			files.push "books"
+		else files.push "0"
+		end
+	elsif entry =~/^  bla/
+		if entry =~ /^.*blank/ 
+			files.push "0"
+		end
+	end
+}
+
+#combine arrays to new array with pairs
+combine = ident.zip(files)
+
+#delete arrays with no text-file
+mapA = []
+mapB = []
+books = []
+
+while combine.length > 0
+	file_pair = combine.pop
+	if file_pair.include?("mapA")
+		mapA.push file_pair
+	elsif 	file_pair.include?("mapB")
+		mapB.push file_pair
+	elsif 	file_pair.include?("books")
+		books.push file_pair
+	end
+end		
+
+#clean up within arrays
+mapA = mapA.flatten!.sort
+mapB = mapB.flatten!.sort
+books = books.flatten!.sort
+
+mapA.delete("mapA") 
+mapB.delete("mapB")
+books.delete("books")
+
+# write arrays to files
+outA = File.open("mapA.txt","w")
+outB = File.open("mapB.txt","w")
+outBook = File.open("books.txt","w")
+
+outA.puts mapA
+outB.puts mapB
+outBook.puts books
+
+outA.close
+outB.close
+outBook.close
+
+
+###### Getting everything together to compare #########
+
+#get array of all copied articles
+copies = mapA + mapB + books
+
+pdfs = IO.readlines ("pdfs.txt")
+ident = IO.readlines("references.txt")
 
 #compare the lists
-pdfs = IO.readlines("articles.txt")
-cites = IO.readlines("references.txt")
-
-extra_pdfs = pdfs - cites
-
-extra_cites = cites - pdfs -copies
-
-extra_copies = copies - cites
+extra_pdfs = pdfs - ident
+extra_cites = ident - pdfs - copies
 
 if extra_pdfs.length  > 0
-out = File.open("D:/Texte/extraPDF.txt", "w")
-out.puts extra_pdfs.sort
+	out = File.open("extraPDF.txt", "w")
+	out.puts extra_pdfs.sort
+	out.close
 end
 
 if extra_cites.length > 0
-out2 = File.open("D:/Texte/extraCITE.txt", "w")
-out2.puts extra_cites.sort
-end
-
-if extra_copies.length > 0
-out3 = File.open("D:/Texte/extraCOPIES.txt", "w")
-out3.puts extra_copies.sort
+	out = File.open("extraCITE.txt", "w")
+	out.puts extra_cites.sort
+	out.close
 end
 
 puts "Amount of pdfs: " + pdfs.length.to_s
 puts "Amount of copies: " +copies.length.to_s
-puts "Amount of citations: " + cites.length.to_s
+puts "Amount of citations: " + ident.length.to_s
 puts "Extra pdfs: " +extra_pdfs.length.to_s
 puts "Extra citations: " +extra_cites.length.to_s
-puts "Extra copies: " + extra_copies.length.to_s
