e890198fe11fe1345db90b942a72341e87305a93
doerte <me@doerte.eu>
2010-08-02 17:54:17 +0200

    improvement

--- 12Ex1.rb
+++ 12Ex1.rb
@@ -1,7 +1,18 @@
 puts 'Please enter your exact time of birth'
-puts 'Use the following layout: '
+puts 'First the year'
+y = gets.chomp
+puts 'Then the month (1-12)'
+m = gets.chomp
+puts 'Now the day (1-31)'
+d = gets.chomp
+puts 'The hour... (0-24)'
+h = gets.chomp
+puts 'The minute... (0-59)'
+mi = gets.chomp
+puts 'And if you know the second... (0-59), write "0" if you don\'t know!'
+s = gets.chomp
 puts 'yyyy, mm, dd, hh, mm, ss'
-birthtime = gets.chomp
-birthtime = Time.local(birthtime)
+birthtime = Time.local(y, m, d, h, mi, s)
+puts birthtime
 puts "You'll be 1 billion seconds on #{birthtime + 1000000000}"
 
