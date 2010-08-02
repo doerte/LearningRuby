b38b3a6620ed8b50a3a9c70fe1578c86bb37e208
doerte <me@doerte.eu>
2010-08-02 23:02:28 +0200

    going on testing

--- ll.rb
+++ ll.rb
@@ -1,15 +1,30 @@
 output = []
-buffer =[]
-reg1 = /^Mk([0-9]+)=Stimulus,(Std.*),([0-9]+),0,0$/
-reg2 = /^Mk([0-9]+)=Stimulus,([^S].*),([0-9]+),0,0$/
+buffer = []
+reg = /^[^Mk]/
+regM = /^Mk([0-9]+)=[^Stimulus]/
+reg1 = /^Mk([0-9]+)=Stimulus,(S  1),([0-9]+),0,0/
+reg2 = /^Mk([0-9]+)=Stimulus,(S  2|S  3|S  4),([0-9]+),0,0/
+reg3 = /^Mk([0-9]+)=Stimulus,(S.*),([0-9]+),0,0/
 
-input = IO.readlines("1_1_AV.vmrk") {|line|
-	if line =~ reg1
-		buffer[0].push line
+input = IO.readlines("15_1_AV.vmrk") 
+
+
+
+input.map {|line|
+	
+	if line =~ reg || line =~ regM
+		output.push line
+	
+	elsif line =~ reg1
+		buffer[0] = line
+		
 	elsif line =~ reg2
 		output.push buffer.to_s
 		output.push line
-		p output
+	
+	elsif line =~ reg3
+		output.push line
+		
 	end
 }
 
