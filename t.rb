048b699c29b3981c070e2cd0ea7749bb60ad83c9
Dörte Hessler <me@doerte.eu>
2010-08-02 08:50:11 +0200

    test krams für bib2.rb

--- t.rb
+++ t.rb
@@ -7,17 +7,13 @@ bib = {}
 ident = nil
 reg = /^\s*@[a-zA-Z]+\{([^,]+),/
 regEntry = /^\s*([^@]+)\s*=\s*\{([^\}]+).*$/#### Make hash of hashes from bibtex file #####
-regLoc = /^\s*file\s*=\s*\{:([a-zA-Z]+).*$/
-regLoc2 =  /^\s*file\s*=\s*\{.*;:([a-zA-Z]+).*$/
+regLoc = /^\s*(file)\s*=\s*\{(:|.*;:)([a-zA-Z]+).*$/
 
 
 File.foreach(ARGV[0]) {|line|
 	if line =~ regLoc # get value of hash (consisting of a hash of type and value)
 		entry = regLoc.match(line)
-		p entry[1]
-	elsif line =~ regLoc2
-		entry2 = regLoc2.match(line)
-		p entry2[1]
+		p "#{entry[1]} = #{entry[3]}"
 	end	
 }
 
