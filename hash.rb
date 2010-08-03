489620dfd749db00724cbd6ce94b53ea91a00b94
Dörte Hessler <me@doerte.eu>
2010-08-03 23:04:41 +0200

    makes array per file of only necessary markers and writes them to output, however no renaming yet

--- hash.rb
+++ hash.rb
@@ -1,3 +1,8 @@
+###################### 
+#Program to reorganize ERP-Trigger #
+######################
+require 'fileutils'
+
 reg = /^[^Mk]/
 regM = /^Mk([0-9]+)=[^Stimulus]/
 reg1 = /^Mk([0-9]+)=Stimulus,(S  1),([0-9]+),0,0/
@@ -5,97 +10,72 @@ reg2 = /^Mk([0-9]+)=Stimulus,(S  2|S  3|S  4),([0-9]+),0,0/
 reg3 = /^Mk([0-9]+)=Stimulus,(S.*),([0-9]+),0,0/
 
 
-array = []
-buffer=[]
-output=[]
-hash = {}
-bla =[]
-blub =[]
-blob =[]
-blub2 = []
 
-File.foreach("15_1_AV.vmrk") {|line|
-	array.push line
-}
+
+##### get files to process ######
+files = Dir["*.vmrk"]
+
+files.each {|file|
+	print "."
+	array = []
+	buffer=[]
+	output=[]
+	finalarray=[]
+	#copy marker files in new directory and switch to it
+	unless File.directory?("NewMarkers") 
+		Dir.mkdir("NewMarkers")
+	end
+	FileUtils.copy(file.to_s, "NewMarkers/#{file.to_s}")
+	
+	Dir.chdir("NewMarkers")
+	#markerID = String.new	
+	
+	#edit marker files in new directory
+	array = IO.readlines(file.to_s)
 	
 
-# header stuff to output and delete from array
-array.each {|entry|
-	if entry =~ reg || entry =~ regM
-		output.push entry
-	end
-}
+	# header stuff to output and delete from array
+	array.each {|entry|
+		if entry =~ reg || entry =~ regM
+			output.push entry
+		end
+	}
+	p output
+	array.delete_if {|x| x =~ reg || x =~ regM}
 
-array.delete_if {|x| x =~ reg || x =~ regM}
-
-# get only entries denoting stimulus type in bla, rest in blub
-array.each {|entry|
-	
-	if array.index == 0 || array.index(entry)%3 == 0
-		bla.push entry 
-	else
-		blub.push entry 
-	end
-	
-}
-
-# split blub into blob and blub2 containing triggers for answer and videoquality
-blub.each {|entry|
-	if blub.index == 0 || blub.index(entry)%2 == 0
-		blob.push entry
-	else blub2.push entry
-	end
-}
-
-p blub2
-
-if bla.length != blub2.length()
-puts "error in bla blub blob" 
-Process.exit
-elsif
-bla.length != blob.length
-puts "error in bla blub blob" 
-Process.exit
-elsif
-blub2.length != blob.length
-puts "error in bla blub blob" 
-Process.exit
-end
-
-# put triggers into hash with stimulus => [answer, vidqual] 
-while bla.length > 0 do 
-
-	hash[bla[0]]= {blob[0],blub2[0]}
-	bla.delete_at(0)
-	bla
-	blob.delete_at(0)
-	blob
-	blub2.delete_at(0)
-
-end
-
-
-
-#array.map {|entry|
-
-	#entry.each_pair {|k, v|
-	#if k== "S  1"
-	#	buffer = []
-	#	buffer.push k
-	#	buffer.push v[0]
-	#	buffer.push v[1]
+	# make triplets of triggers in big array
+		while array.length > 0 do
+		buffer[0] = array[0]
+		buffer[1] = array[1]
+		buffer[2] = array[2]
+		finalarray.push buffer
 		
-	#else
-	#	output = buffer
-	#	output.push k
-	#	output.push v[0]
-	#	output.push v[1]
-		
-	#end
+		buffer = []
+		array.delete_at(0)
+		array.delete_at(0)
+		array.delete_at(0)
+	end
 	
-#}
+
+	# reverse array
+	finalarray.reverse!
+	finalarray
 
 
-out = File.open("text.txt","w")
-out.puts hash
-out.close
+	#keep only triplets of triggers S2,S3 and S4 as well as S1 if preceded by deviant!
+	finalarray.each {|a|
+		if a[0] =~ reg2 
+			output.push a
+		elsif a[0] =~ reg1 && buffer[0]=~reg2
+			output.push a
+		end
+		buffer[0] = a[0]
+	}
+
+
+	#write to file
+	out = File.open(file.to_s,"w")
+	out.puts output
+	out.close
+	Dir.chdir("..")
+}
