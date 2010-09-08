f378d50e3876f788170e01714724baad97176f3f
doerte <me@doerte.eu>
2010-09-08 14:41:06 +0200

    deleted unnecessary row

--- 12Ex1.rb
+++ 12Ex1.rb
@@ -11,7 +11,6 @@ puts 'The minute... (0-59)'
 mi = gets.chomp
 puts 'And if you know the second... (0-59), write "0" if you don\'t know!'
 s = gets.chomp
-puts 'yyyy, mm, dd, hh, mm, ss'
 birthtime = Time.local(y, m, d, h, mi, s)
 puts birthtime
 puts "You'll be 1 billion seconds on #{birthtime + 1000000000}"
